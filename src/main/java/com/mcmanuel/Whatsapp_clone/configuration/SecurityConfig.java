a27e4e12933a5ef9a27d239c062f2505e9f3ffff
unknown <mcmanuel755@gmail.com>
2025-05-09 15:59:58 +0100

    security config class updated

--- src/main/java/com/mcmanuel/Whatsapp_clone/configuration/SecurityConfig.java
+++ src/main/java/com/mcmanuel/Whatsapp_clone/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.mcmanuel.Whatsapp_clone.configuration;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -39,5 +46,28 @@ public class SecurityConfig {
                                 token->
                                         token.jwtAuthenticationConverter(new KeyCloakJwtAuthenticationConverter())))
                 .build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://loaclhost:4200"));
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.ORIGIN,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT,
+                HttpHeaders.AUTHORIZATION
+                ));
+        config.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "DELETE",
+                "OPTIONS",
+                "PATCH"
+        ));
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter(source);
     }
 }
