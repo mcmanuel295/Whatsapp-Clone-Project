dd6948e82d7494443a73a878ec80efad91fde308
unknown <mcmanuel755@gmail.com>
2025-05-29 00:54:14 +0100

    changes made

--- src/main/java/com/mcmanuel/Whatsapp_clone/file/FileService.java
+++ src/main/java/com/mcmanuel/Whatsapp_clone/file/FileService.java
@@ -1,11 +1,25 @@
 package com.mcmanuel.Whatsapp_clone.file;
 
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 @Service
 public class FileService {
-    public String saveFile(MultipartFile file, String senderId) {
-        return null;
+
+    @Value("${Application.file.uploads.media-output.path}")
+    private String fileUploaded;
+
+    public String saveFile(@NonNull MultipartFile sourceFile, @NonNull  String senderId) {
+
+        final String  fileUploadSubPath = "user"+ File.pathSeparator+senderId;
+        return uploadFile(sourceFile,fileUploadSubPath);
+    }
+
+    private String uploadFile(@NonNull MultipartFile sourceFile, @NonNull String fileUploadSubPath) {
+        final Strinf uploadPath = fileUploadSubPath+File.pathSeparator+fileUploadSubPath;
     }
 }
